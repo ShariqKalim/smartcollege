6cf05645129a426e4d24d63cbffdca658b440679
Shariq Kalim <shariqkalim123@gmail.com>
2021-09-06 01:54:06 +0530

    successfully activated sem 3 and changed headings

--- src/components/Classnotes.js
+++ src/components/Classnotes.js
@@ -10,7 +10,7 @@ const Classnotes = () => {
           className="no-underline transform hover:scale-110"
         >
           <div className="responsiveBoxes rounded-2xl border-4 bg-gradient-to-tl from-yellow-500 via-yellow-400 to-yellow-500 shadow-lg p-10 w-96">
-            <h1 className="text-white tracking-wide">CSE</h1>
+            <h2 className="text-white tracking-wide">CSE</h2>
             <p className="text-white font-normal">
               If you are from Computer Science and Engineering Department then
               select this block
@@ -22,7 +22,7 @@ const Classnotes = () => {
           className="no-underline transform hover:scale-110"
         >
           <div className="responsiveBoxes rounded-2xl border-4 bg-gradient-to-tl from-green-500 via-green-400 to-green-500 shadow-lg p-10 w-96 ">
-            <h1 className="text-white tracking-wide">IT</h1>
+            <h2 className="text-white tracking-wide">IT</h2>
             <p className="text-white font-normal">
               If you are from Information Technology Department then select this
               block
@@ -34,7 +34,7 @@ const Classnotes = () => {
           className="no-underline transform  hover:scale-110"
         >
           <div className="responsiveBoxes rounded-2xl border-4 bg-gradient-to-tl from-red-500 via-red-400 to-red-500 opacity-90 shadow-lg p-10 w-96 ">
-            <h1 className="text-white tracking-wide">ECE</h1>
+            <h2 className="text-white tracking-wide">ECE</h2>
             <p className="text-white font-normal">
               If you are from Electronics and Communication Engineering
               Department then select this block
@@ -46,7 +46,7 @@ const Classnotes = () => {
           className="no-underline transform hover:scale-110"
         >
           <div className="responsiveBoxes rounded-2xl border-4 bg-gradient-to-tl from-blue-500 via-blue-400 to-blue-500 shadow-lg p-10 w-96 mt-10">
-            <h1 className="text-white tracking-wide">EE</h1>
+            <h2 className="text-white tracking-wide">EE</h2>
             <p className="text-white font-normal">
               If you are from Electrical Engineering Department then select this
               block
